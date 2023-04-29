ee22d8f802369b82808efb5b2f27e3ae0be60701
Noah Ralph <NoahJRalph@gmail.com>
2023-04-28 21:01:22 -0700

    Full product

---
+++ README.md
@@ -0,0 +1,36 @@
+# nodejs-readme-generator
+
+![This project's protection license.](https://img.shields.io/badge/license-MIT-blue)
+
+## Table of Contents
+
+- [Description](#Description)
+- [Installation](#Deployment)
+- [Usage](#Usage)
+- [License](#License)
+- [Questions?](#Questions?)
+
+## Description
+
+My very own simple Day Planner. Made using the criteria from Module 5 of my edX Web Development bootcamp.
+
+## Deployment
+
+![Here.]https://noahjralph.github.io/work-day-planner/
+
+## Usage
+
+Each hour is labeled: Gray(past), Red(present), or Green(future).
+On the deployed page, insert reminders or data into the text blocks next to each hour's time, then hit the save button. This will ensure the site will keep the inputed data.
+
+## Demonstration
+
+![A gif demonstrating the unmatched power of Noah's Day Planner.](./assets/imgs/demo.PNG)
+
+## License
+
+This project is protected under the MIT License.
+
+## Questions?
+
+Reach out to me either on [GitHub](https://github.com/NoahJRalph) or by [Email](mailto:NoahJRalph@gmail.com)!
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  let masterContainerEl = $(`#masterContainer`);
-  let saveBtnEl = $(`#saveBtn`);
-  saveBtnEl.on(`click`, function () {
-    let ParentDivID = $(this).parent().parent().attr('id');
-    console.log($(this).parent().parent().attr('id'));
-  });
-  //Populates the rows with adequately time-labeled bars while keeping the code as clean as possible (not copy-pasting 24 different rowContents here or in the html)
-  console.log(dayjs().hour());
+$(document).ready(function () {
+  //Populates the rows with adequately time-labeled bars while keeping the code as clean as possible (not copy-pasting 24 different rowContents here or in the html).
   for(let i=0; i<24; i++) {
-    // Sets correct hour text
+    // Sets correct hour text.
     let hour = i;
     let amPM = `AM`;
     switch(i) {
@@ -31,7 +18,7 @@ $(function () {
       hour = i-12;
       amPM = `PM`;
     }
-    // Sets correct bar color
+    // Sets correct bar color.
     let timeLabel = [`past`, `present`, `future`];
     let timeChoice;
     if(dayjs().hour() > i) {
@@ -43,24 +30,29 @@ $(function () {
     else {
       timeChoice = 2;
     }
-    // Takes all the data and makes a proper html blurb with it to insert
+    // Sets the textarea content.
+    let textAreaContent = ``;
+    let storedHourInfo = localStorage.getItem(`hour-${i}`);
+    if (storedHourInfo != null && storedHourInfo != undefined) {
+      textAreaContent = storedHourInfo;
+    }
+    // Takes all the data and makes a proper html blurb with it to insert.
     let rowContent = (
-      `<div id="hour-` + i + `" class="row time-block ` + timeLabel[timeChoice] + `">
-        <div class="col-2 col-md-1 hour text-center py-3">` + hour + amPM + `</div>
-        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+      `<div id="hour-${i}" class="row time-block ${timeLabel[timeChoice]}">
+        <div class="col-2 col-md-1 hour text-center py-3">${hour}${amPM}</div>
+        <textarea class="col-8 col-md-10 description" rows="3">${textAreaContent}</textarea>
         <button class="btn saveBtn col-2 col-md-1" aria-label="save">
           <i class="fas fa-save" aria-hidden="true"></i>
         </button>
       </div>`
     );
-    masterContainerEl.append(rowContent);
-  }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // Displays current day's date
-  let currentDay = dayjs().format('MMM DD, YYYY');
-  let currentDayEl = $(`#currentDay`);
-  currentDayEl.text(currentDay);
+    $(`#masterContainer`).append(rowContent);
+  };
+  // ===OUTSIDE THE HOUR RENDER LOOP===
+  // Displays current day's date.
+  $(`#currentDay`).text(dayjs().format(`MMM DD, YYYY`));
+  // Saves data inputed into the textarea when the save button is hit.
+  $(`.saveBtn`).on(`click`, function () {
+    localStorage.setItem($(this).parent().attr(`id`), $(this).siblings(`.description`).val());
+  });
 });
